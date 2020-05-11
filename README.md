6252267057fec558611b780410610b6d5f468eeb
Juggernaut9 <xbhavyatomar@gmail.com>
2020-05-12 00:22:06 +0530

    chained forecast and geocode callbacks, now input location as argument

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+**How to use -**
+	$ node app.js "new york"
+	$ node app.js "paris"
+
 module for request - **postman-request**
 
 request is deprecated (but still works)
--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const request = require('postman-request')
-
 const forecast = require('./utils/forecast.js')
-
 const geocode = require('./utils/geocode.js')
 
+const location = process.argv[2]
 
-// geocode('ajmer',(error, data)=>{
-//     console.log('error', error)
-//     console.log('data',data)
-// })
-
-
-
-forecast(37.8267,-122.4233, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
-
+if (!location){
+    console.log("Please enter a location")
+}
+else{
+    geocode(process.argv[2],(error, data)=>{
+        if (error){
+            console.log(error)
+        }
+    
+        forecast(data.latitude,data.longitude, (error, forecastData) => {
+            if (error){
+                return console.log(error)
+            }    
+    
+            console.log(data.location)
+            console.log(forecastData)
+    
+        })    
+    })    
+}
